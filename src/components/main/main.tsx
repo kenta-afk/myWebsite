d2167c219a258aab4fc10a1f9d3a94c5f2dcad49
土居健太郎 <kentarou9.60@icloud.com>
2025-02-04 23:12:38 +0900

    ref

---
+++ src/components/main/main.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+
+export const Main = component$(() => {
+    return (
+        <main>
+            <h1 id="about">about</h1>
+            <h1 id="works">works</h1>
+        </main>
+    )
+})
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Header } from "../components/header/header";
+import { Main } from "../components/main/main";
 import "../components/header/style.css";
+import "../components/main/style.css";
 
 export default component$(() => {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 });
